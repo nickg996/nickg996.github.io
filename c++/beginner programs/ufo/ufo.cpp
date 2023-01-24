05ad9d492243ae238d7c8f8a5c33ecb210c42145
nickg996 <75364307+nickg996@users.noreply.github.com>
2023-01-24 13:56:30 -0800

    cleaning up UFO code

--- c++/beginner programs/ufo/ufo.cpp
+++ c++/beginner programs/ufo/ufo.cpp
@@ -2,6 +2,7 @@
 #include "ufo_functions.hpp"
 
 int main() {
+
   greet();
 
   std::string codeword = "codecademy";
@@ -11,39 +12,41 @@ int main() {
   bool guess = false;
   char letter;
 
-  while (answer != codeword && misses < 7)
-  {
+  while (answer != codeword && misses < 7) {
+    
     display_misses(misses);
     display_status(incorrect, answer);
+
     std::cout << "\n\nPlease enter your guess: ";
     std::cin >> letter;
 
-    for (int i = 0; i < codeword.size(); i++)
-    {
-      if (letter == codeword[i])
-      {
+    for (int i = 0; i < codeword.length(); i++) {
+
+      if (letter == codeword[i]) {
+
         answer[i] = letter;
         guess = true;
+
       }
-    }
-    if (guess = true)
-    {
-      std::cout << "\nCorrect!\n";
+
     }
 
-    else
-    {
-      std::cout << "\nIncorrect! The tractor beam pulls the person in further. ";
+    if (guess) {
+
+      std::cout << "\nCorrect!\n";
+
+    } else {
+
+      std::cout << "\nIncorrect! The tractor beam pulls the person in further.\n";
       incorrect.push_back(letter);
       misses++;
+
     }
 
-      guess = false;
+    guess = false;
+    
   }
 
   end_game(answer, codeword);
-  
+
 }
-
-
-
--- c++/beginner programs/ufo/ufo_functions.hpp
+++ c++/beginner programs/ufo/ufo_functions.hpp
@@ -2,45 +2,44 @@
 #include <vector>
 
 // Define functions
-void greet()
-{
-  std::cout << "============\n";
+void greet() {
+
+  std::cout << "=============\n";
   std::cout << "UFO: The Game\n";
-  std::cout << "============\n";
-  std::cout << "Instructions: Save your friend from alien abduction by guessing the letters in the codeword.\n";
+  std::cout << "=============\n";
+  std::cout << "Instructions: save your friend from alien abduction by guessing the letters in the codeword.\n";
+
 }
 
-void display_status(std::vector<char> incorrect, std::string answer)
-{
-  std::cout << "\nIncorrect Guesses: \n";
-    for (int i = 0; i < incorrect.size(); i++)
-    {
-      std::cout << incorrect[i] << " ";
-    }
+void display_status(std::vector<char> incorrect, std::string answer) {
 
-    std::cout << "\nCodeword: \n";
-    for (int i = 0; i < answer.size(); i++)
-    {
-      std::cout << answer[i] << " ";
-    }
-}
+  std::cout << "\nIncorrect Guesses:\n";
 
+  for (int i = 0; i < incorrect.size(); i++) {
+    std::cout << incorrect[i] << ' ';
+  }
+  
+  std::cout << "\nCodeword:\n";
 
-void end_game(std::string answer, std::string codeword)
-{
-  if (answer == codeword)
-  {
-    std::cout << "Hooray! You saved the pereson and earned a medal of honor!";
+  for (int i = 0; i < answer.length(); i++) {
+    std::cout << answer[i] << ' ';
   }
 
-  else 
-  {
-    std::cout << "Oh no! The UFO just flew away with another person!";
-  }
 }
 
+void end_game(std::string answer, std::string codeword) {
 
+  if (answer == codeword) {
 
+    std::cout << "Hooray! You saved the person and earned a medal of honor!\n";
+
+  } else {
+
+    std::cout << "Oh no! The UFO just flew away with another person!\n";
+
+  }
+
+}
 
 void display_misses(int misses) {
 
